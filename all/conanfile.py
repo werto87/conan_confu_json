0eceee266af11cd5f395278f8d12412b405bdbf4
werto87 <huhulasmichinruhe@web.de>
2021-07-19 16:48:39 +0200

    update to new recipe

--- all/conanfile.py
+++ all/conanfile.py
@@ -1,6 +1,8 @@
 from conans import ConanFile, tools
 from conans.tools import check_min_cppstd
+from conans.errors import ConanInvalidConfiguration, ConanException
 import os
+from conans.tools import Version
 
 
 class ConfuJson(ConanFile):
@@ -17,9 +19,38 @@ class ConfuJson(ConanFile):
     def _source_subfolder(self):
         return "source_subfolder"
 
+    @property
+    def _minimum_cpp_standard(self):
+        return 20
+
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "15",
+            "gcc": "10",
+            "clang": "10",
+        }
+
     def configure(self):
-        if self.settings.compiler.cppstd:
-            check_min_cppstd(self, "20")
+        if self.settings.compiler == "apple-clang":
+            raise ConanInvalidConfiguration(
+                "apple-clang is not supported because of missing concept support")
+        if self.settings.compiler.get_safe("cppstd"):
+            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+        min_version = self._minimum_compilers_version.get(
+            str(self.settings.compiler))
+        if not min_version:
+            self.output.warn("{} recipe lacks information about the {} "
+                             "compiler support.".format(
+                                 self.name, self.settings.compiler))
+        else:
+            if tools.Version(self.settings.compiler.version) < min_version:
+                raise ConanInvalidConfiguration(
+                    "{} requires C++{} support. "
+                    "The current compiler {} {} does not support it.".format(
+                        self.name, self._minimum_cpp_standard,
+                        self.settings.compiler,
+                        self.settings.compiler.version))
         self.options["boost"].header_only = True
 
     def requirements(self):
@@ -32,9 +63,9 @@ class ConfuJson(ConanFile):
         os.rename(extracted_dir, self._source_subfolder)
 
     def package(self):
-        # This should lead to an Include path like #include "include_folder/IncludeFile.hxx"
         self.copy("*.h*", dst="include/confu_json",
                   src="source_subfolder/confu_json")
+        self.copy("*LICENSE.md", dst="licenses", keep_path=False)
 
     def package_id(self):
         self.info.header_only()
