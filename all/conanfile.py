88cbbb09e6056b8a96747a23a79fd310b450bebb
werto87 <huhulasmichinruhe@web.de>
2023-01-10 15:55:56 +0900

    new version

--- all/conanfile.py
+++ all/conanfile.py
@@ -21,20 +21,18 @@ class ConfuJson(ConanFile):
 
     @property
     def _minimum_cpp_standard(self):
-        return 20
+        return 17
 
     @property
     def _minimum_compilers_version(self):
         return {
             "Visual Studio": "15",
-            "gcc": "10",
-            "clang": "10",
+            "gcc": "7",
+            "clang": "3.9",
+            "apple": "10.0",
         }
 
     def configure(self):
-        if self.settings.compiler == "apple-clang":
-            raise ConanInvalidConfiguration(
-                "apple-clang is not supported because of missing concept support")
         if self.settings.compiler.get_safe("cppstd"):
             tools.check_min_cppstd(self, self._minimum_cpp_standard)
         min_version = self._minimum_compilers_version.get(
@@ -51,7 +49,6 @@ class ConfuJson(ConanFile):
                         self.name, self._minimum_cpp_standard,
                         self.settings.compiler,
                         self.settings.compiler.version))
-        self.options["boost"].header_only = True
 
     def requirements(self):
         self.requires("boost/1.78.0")
